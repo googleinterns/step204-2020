ea6932b11fcc1ee42811c9bdc267f9acef699c62
Riya Narayan <riya.narayan@gmail.com>
2020-07-01 20:04:45 +0800

    get date using isoString

--- walk-in-interview/src/main/webapp/script.js
+++ walk-in-interview/src/main/webapp/script.js
@@ -2,27 +2,23 @@
 /** Dynamically add the limits for choosing the new job post expiry. */
 function addJobExpiryLimits() {
   const date = new Date();
-  const year = date.getFullYear();
-  const month= date.getMonth();
-  const day = date.getDate();
-  const min = year.concat('-', month, '-', day);
-  const max = (year + 1).concat('-', month, '-', day);
+  const min = date.toISOString().substr(0, 10);
+  date.setFullYear(date.getFullYear() + 1);
+  const max = date.toISOString().substr(0, 10);
 
-
-  console.log('in here');
   const datePicker = document.getElementById('new-post-expiry');
-  console.log('datepicker', datePicker);
   datePicker.setAttribute('min', min);
   datePicker.setAttribute('max', max);
 }
 
-/** Adds a new job listing by making a POST request to the /jobs servlet */
-// function addJob() {
-//   fetch('/jobs', {
-//     method: 'POST',
-//     headers: {'Content-Type': 'application/json'},
-//     body: JSON.stringify(jobDetails),
-//   }).then((response) => response.text()).then((data) => {
 
-//   });
-// }
+/** Adds a new job listing by making a POST request to the /jobs servlet */
+function addJob() {
+  fetch('/jobs', {
+    method: 'POST',
+    headers: {'Content-Type': 'application/json'},
+    body: JSON.stringify(jobDetails),
+  }).then((response) => response.text()).then((data) => {
+
+  });
+}
