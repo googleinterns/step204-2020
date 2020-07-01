c739ef320748dc3afa82e4ea076253fe5372d5f0
Riya Narayan <riya.narayan@gmail.com>
2020-07-01 22:03:54 +0800

    add constants

--- walk-in-interview/src/main/webapp/script.js
+++ walk-in-interview/src/main/webapp/script.js
@@ -1,4 +1,17 @@
 
+const NEW_JOB_FORM_ID = 'new-job-form';
+const NEW_JOB_TITLE_ID = 'new-job-title';
+const NEW_JOB_ADDRESS_ID = 'new-job-address';
+const NEW_JOB_DESCRIPTION_ID = 'new-job-description';
+const NEW_JOB_PAY = {
+  FREQUENCY_ID: 'new-job-pay-frequency',
+  MIN_ID: 'new-job-pay-min',
+  MAX_ID: 'new-job-pay-max',
+};
+const REQUIREMENTS_LIST_ID = 'requirements-list';
+const NEW_JOB_EXPIRY_ID = 'new-job-expiry';
+const NEW_JOB_DURATION_ID = 'new-job-duration';
+
 /**
  * Adds a new job listing by making a POST request to the /jobs servlet.
  * @return {boolean} If the request given the parameters was valid or not.
@@ -6,28 +19,28 @@
 function addJob() {
   event.preventDefault();
 
-  const formElements = document.forms['new-job-form'];
+  const formElements = document.forms[NEW_JOB_FORM_ID];
   console.log('forrm', formElements.elements);
 
-  const name = formElements['new-job-title'].value;
-  const address = formElements['new-job-address'].value;
-  const description = formElements['new-job-description'].value;
-  const payFrequency = formElements['new-job-pay-frequency'].value;
+  const name = formElements[NEW_JOB_TITLE_ID].value;
+  const address = formElements[NEW_JOB_ADDRESS_ID].value;
+  const description = formElements[NEW_JOB_DESCRIPTION_ID].value;
+  const payFrequency = formElements[NEW_JOB_PAY.FREQUENCY_ID].value;
 
-  const payMin = formElements['new-job-pay-min'].valueAsNumber;
-  const payMax = formElements['new-job-pay-max'].valueAsNumber;
+  const payMin = formElements[NEW_JOB_PAY.MIN_ID].valueAsNumber;
+  const payMax = formElements[NEW_JOB_PAY.MAX_ID].valueAsNumber;
   if (payMin > payMax) {
     return false;
   }
 
-  const requirementsCheckboxes = formElements['requirements-list'];
+  const requirementsCheckboxes = formElements[REQUIREMENTS_LIST_ID];
   const requirementsList = [];
   requirementsCheckboxes.forEach(({checked, id}) => {
       checked? requirementsList.push(id) : '';
   });
 
-  const expiry = formElements['new-job-expiry'].value;
-  const duration = formElements['new-job-duration'].value;
+  const expiry = formElements[NEW_JOB_EXPIRY_ID].value;
+  const duration = formElements[NEW_JOB_DURATION_ID].value;
 
   const jobDetails = {
     jobName: name,
