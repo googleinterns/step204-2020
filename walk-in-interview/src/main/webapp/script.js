07168a09b7e13ce3a856a44bfa378edb5b024da8
Riya Narayan <riya.narayan@gmail.com>
2020-07-02 17:44:16 +0800

    add job duration options dynamically

--- walk-in-interview/src/main/webapp/script.js
+++ walk-in-interview/src/main/webapp/script.js
@@ -1,4 +1,16 @@
 
+/**
+ * Options for job duration.
+ * @enum {stirng}
+ */
+const JOB_DURATION = {
+  ONE_WEEK: '1 Week',
+  TWO_WEEKS: '2 Weeks',
+  ONE_MONTH: '1 Month',
+  SIX_MONTHS: '6 Months',
+  ONE_YEAR: '1 Year',
+};
+
 /** Dynamically add the limits for choosing the new job post expiry. */
 function addJobExpiryLimits() {
   const date = new Date();
@@ -9,4 +21,18 @@ function addJobExpiryLimits() {
   const datePicker = document.getElementById('new-post-expiry');
   datePicker.setAttribute('min', min);
   datePicker.setAttribute('max', max);
+}
+
+/** Dynamically add the options for job duration. */
+function addJobDurationOptions() {
+  const jobDurationSelect = document.getElementById('new-job-duration');
+  jobDurationSelect.options.length = 0;
+
+  jobDurationSelect.options[0] = new Option('Other', '');
+  for (key in JOB_DURATION) {
+    if ({}.hasOwnProperty.call(JOB_DURATION, key)) {
+      jobDurationSelect.options[jobDurationSelect.options.length] =
+      new Option(JOB_DURATION[key], key);
+    }
+  }
 }
