1d921f7d4292dacfb83179c3d1c734bb62e20183
Wu Sibing <1915563868@qq.com>
2020-07-03 16:19:41 +0800

    Add security check for job id

--- walk-in-interview/src/main/java/com/google/job/data/JobsDatabase.java
+++ walk-in-interview/src/main/java/com/google/job/data/JobsDatabase.java
@@ -3,26 +3,18 @@ package com.google.job.data;
 import com.google.api.core.ApiFunction;
 import com.google.api.core.ApiFuture;
 import com.google.api.core.ApiFutures;
-import com.google.cloud.firestore.DocumentReference;
-import com.google.cloud.firestore.DocumentSnapshot;
-import com.google.cloud.firestore.Firestore;
-import com.google.cloud.firestore.WriteResult;
+import com.google.cloud.firestore.*;
 import com.google.common.util.concurrent.MoreExecutors;
 import com.google.firestore.FireStoreUtils;
 import org.checkerframework.checker.nullness.compatqual.NullableDecl;
 
 import java.util.Optional;
 import java.util.concurrent.Future;
+import java.util.concurrent.atomic.AtomicBoolean;
 
 /** Helps persist and retrieve job posts. */
 public final class JobsDatabase {
     private static final String JOB_COLLECTION = "Jobs";
-
-//    private final Firestore firestore;
-//
-//    public JobsDatabase(Firestore firestore) {
-//        this.firestore = firestore;
-//    }
 
     /**
      * Adds a newly created job post.
@@ -42,12 +34,15 @@ public final class JobsDatabase {
      * @param jobId Id for the target job post in the database.
      * @param job Updated job post.
      * @return A future of the detailed information of the update.
+     * @throws IllegalArgumentException If the job id is invalid.
      */
-    public Future<WriteResult> setJob(String jobId, Job job) {
+    public Future<WriteResult> setJob(String jobId, Job job) throws IllegalArgumentException {
         // Overwrites the whole job post
-        return FireStoreUtils.getFireStore()
-                .collection(JOB_COLLECTION).document(jobId)
-                .set(job);
+        DocumentReference docRef = FireStoreUtils.getFireStore().collection(JOB_COLLECTION).document(jobId);
+        if (!isDocumentExist(docRef)) {
+            throw new IllegalArgumentException("Job Id does not exist");
+        }
+        return docRef.set(job);
     }
 
     /**
@@ -55,10 +50,15 @@ public final class JobsDatabase {
      *
      * @param jobId Id for the job post in the database.
      * @return Future of the target job post.
+     * @throws IllegalArgumentException If the job id is invalid.
      */
-    public Future<Optional<Job>> fetchJob(String jobId) {
+    public Future<Optional<Job>> fetchJob(String jobId) throws IllegalArgumentException {
         DocumentReference docRef = FireStoreUtils.getFireStore()
                 .collection(JOB_COLLECTION).document(jobId);
+
+        if (!isDocumentExist(docRef)) {
+            throw new IllegalArgumentException("Job Id does not exist");
+        }
 
         // Asynchronously retrieves the document
         ApiFuture<DocumentSnapshot> snapshotFuture = docRef.get();
@@ -71,5 +71,12 @@ public final class JobsDatabase {
         };
 
         return ApiFutures.transform(snapshotFuture, jobFunction, MoreExecutors.directExecutor());
+    }
+
+    private boolean isDocumentExist(DocumentReference docIdRef) {
+        AtomicBoolean isExist = new AtomicBoolean(/* initialValue= */false);
+        docIdRef.addSnapshotListener((documentSnapshot, e) -> isExist.set(documentSnapshot.exists()));
+
+        return isExist.get();
     }
 }
