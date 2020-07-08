8495550e188ad66910caa481e4c834574efee077
Wu Sibing <1915563868@qq.com>
2020-07-08 15:58:38 +0800

    Match enum with string id

--- walk-in-interview/src/main/java/com/google/job/data/JobDuration.java
+++ walk-in-interview/src/main/java/com/google/job/data/JobDuration.java
@@ -2,9 +2,45 @@ package com.google.job.data;
 
 /** Enumeration for duration of a job. */
 public enum JobDuration {
-    ONE_WEEK,
-    TWO_WEEKS,
-    ONE_MONTH,
-    SIX_MONTHS,
-    ONE_YEAR
+    ONE_WEEK("ONE_WEEK"),
+    TWO_WEEKS("TWO_WEEKS"),
+    ONE_MONTH("ONE_MONTH"),
+    SIX_MONTHS("SIX_MONTHS"),
+    ONE_YEAR("ONE_YEAR"),
+    OTHER("OTHER");
+
+    private final String duration;
+
+    JobDuration(String duration) {
+        this.duration = duration;
+    }
+
+    /**
+     * Gets the String id matching the enum value.
+     *
+     * @return String id matching the enum value.
+     */
+    public String getDuration() {
+        return duration;
+    }
+
+    /**
+     * Gets the name of the enum value.
+     *
+     * @return Name of the enum value.
+     */
+    public String getDurationId() {
+        return this.name();
+    }
+
+    /**
+     * Returns the duration enum matching the provided id.
+     *
+     * @param id String id matching the enum value.
+     * @return Duration enum matching the provided id.
+     * @throws IllegalArgumentException If a duration cannot be found for provided id.
+     */
+    public static JobDuration getFromId(String id) throws IllegalArgumentException {
+        return JobDuration.valueOf(id);
+    }
 }
--- walk-in-interview/src/main/java/com/google/job/data/PaymentFrequency.java
+++ walk-in-interview/src/main/java/com/google/job/data/PaymentFrequency.java
@@ -2,8 +2,43 @@ package com.google.job.data;
 
 /** Enumeration that represents the payment frequency. */
 public enum PaymentFrequency {
-    HOURLY,
-    WEEKLY,
-    MONTHLY,
-    YEARLY
+    HOURLY("HOURLY"),
+    WEEKLY("WEEKLY"),
+    MONTHLY("MONTHLY"),
+    YEARLY("YEARLY");
+
+    private final String frequency;
+
+    PaymentFrequency(String duration) {
+        this.frequency = duration;
+    }
+
+    /**
+     * Gets the String id matching the enum value.
+     *
+     * @return String id matching the enum value.
+     */
+    public String getFrequency() {
+        return frequency;
+    }
+
+    /**
+     * Gets the name of the enum value.
+     *
+     * @return Name of the enum value.
+     */
+    public String getFrequencyId() {
+        return this.name();
+    }
+
+    /**
+     * Returns the payment frequency enum matching the provided id.
+     *
+     * @param id String id matching the enum value.
+     * @return Frequency enum matching the provided id.
+     * @throws IllegalArgumentException If a payment frequency cannot be found for provided id.
+     */
+    public static PaymentFrequency getFromId(String id) throws IllegalArgumentException {
+        return PaymentFrequency.valueOf(id);
+    }
 }
