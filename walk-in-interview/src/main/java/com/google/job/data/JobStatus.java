325e28594e67a9f63b9518adbd2b21fbeebd6490
Wu Sibing <1915563868@qq.com>
2020-07-08 16:03:24 +0800

    Add string id for JobStatus enum

--- walk-in-interview/src/main/java/com/google/job/data/JobStatus.java
+++ walk-in-interview/src/main/java/com/google/job/data/JobStatus.java
@@ -2,7 +2,42 @@ package com.google.job.data;
 
 /** Enumeration that represents the status of a job post. */
 public enum JobStatus {
-    ACTIVE,
-    DELETED,
-    EXPIRED
+    ACTIVE("ACTIVE"),
+    DELETED("DELETED"),
+    EXPIRED("EXPIRED");
+
+    private final String status;
+
+    JobStatus(String status) {
+        this.status = status;
+    }
+
+    /**
+     * Gets the String id matching the enum value.
+     *
+     * @return String id matching the enum value.
+     */
+    public String getStatus() {
+        return status;
+    }
+
+    /**
+     * Gets the name of the enum value.
+     *
+     * @return Name of the enum value.
+     */
+    public String getStatusId() {
+        return this.name();
+    }
+
+    /**
+     * Returns the job status enum matching the provided id.
+     *
+     * @param id String id matching the enum value.
+     * @return Status enum matching the provided id.
+     * @throws IllegalArgumentException If a status cannot be found for provided id.
+     */
+    public static JobStatus getFromId(String id) throws IllegalArgumentException {
+        return JobStatus.valueOf(id);
+    }
 }
