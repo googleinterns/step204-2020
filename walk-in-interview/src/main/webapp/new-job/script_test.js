0be089e3191cda78e7320b5cfa5445b748e1ca5b
Riya Narayan <riya.narayan@gmail.com>
2020-07-09 18:17:41 +0800

    mocha selenium successfully integrated

--- walk-in-interview/src/main/webapp/new-job/script_test.js
+++ walk-in-interview/src/main/webapp/new-job/script_test.js
@@ -7,15 +7,44 @@
 
 const assert = require('assert');
 
-// Create a test group named Math
-// Create two tests within the group Math.
-// Test one: Should test if 3*3 = 9
-// Test two: Should test if (3–4)*8 = -8
+
+const webdriver = require('selenium-webdriver');
+const chrome = require('selenium-webdriver/chrome');
+const path = require('chromedriver').path;
+
+const service = new chrome.ServiceBuilder(path).build();
+chrome.setDefaultService(service);
+
+const By = webdriver.By;
+const until = webdriver.until;
+
+let driver;
+
 describe('Math', function() {
+  this.timeout(50000);
+  beforeEach(async () => {
+    driver = await new webdriver.Builder()
+        .withCapabilities(webdriver.Capabilities.chrome())
+        .build();
+    await driver.get('https://library-app.firebaseapp.com/');
+  });
+
+  afterEach(async () => {
+    await driver.quit();
+  });
+
   it('should test if 3*3 = 9', function() {
     assert.equal(9, 3*3);
   });
   it('should test if (3-4)*8=-8', function() {
     assert.equal(-8, (3-4)*8);
+  });
+
+
+  it('changes button opacity on user input', async () => {
+    await driver.findElement(By.css('input')).sendKeys('us@goog.com')
+        .catch((err) => {
+          console.log('error:', err);
+        });
   });
 });
