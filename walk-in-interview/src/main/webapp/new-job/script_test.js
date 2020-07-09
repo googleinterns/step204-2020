479cb3e25cc52f66a95fcafeb4a957bc7c56e3b2
Riya Narayan <riya.narayan@gmail.com>
2020-07-09 19:52:42 +0800

    add render tests for header elements

--- walk-in-interview/src/main/webapp/new-job/script_test.js
+++ walk-in-interview/src/main/webapp/new-job/script_test.js
@@ -1,13 +1,22 @@
 /**
  * This file is where we will write tests for the
- * funcions in new-job/script.js using mocha.
+ * funcions in new-job/script.js using mocha and selenium.
  * Running `npm test` in the command line will run all
  * the tests in the walk-in-interview directory.
  */
 
+// TODO(issue/21): get the language from the browser
+const CurrentLocale = 'en';
+
+/**
+ * Import statements are static so its parameters cannot be dynamic.
+ * TODO(issue/22): figure out how to use dnamic imports
+ */
+import {AppStrings} from './strings.en.js';
+
+const STRINGS = AppStrings['new-job'];
+
 const assert = require('assert');
-
-
 const webdriver = require('selenium-webdriver');
 const chrome = require('selenium-webdriver/chrome');
 const path = require('chromedriver').path;
@@ -20,31 +29,67 @@ const until = webdriver.until;
 
 let driver;
 
-describe('Math', function() {
+describe('New Job Tests', () => {
   this.timeout(50000);
-  beforeEach(async () => {
-    driver = await new webdriver.Builder()
+
+  beforeEach(() => {
+    driver = new webdriver.Builder()
         .withCapabilities(webdriver.Capabilities.chrome())
         .build();
-    await driver.get('https://library-app.firebaseapp.com/');
+    // TODO(issue/31): figure out how to test our local host url
+    return driver.get('https://riyanar-step-2020.appspot.com/');
   });
 
-  afterEach(async () => {
-    await driver.quit();
+  afterEach(() => {
+    return driver.quit();
   });
 
-  it('should test if 3*3 = 9', function() {
-    assert.equal(9, 3*3);
-  });
-  it('should test if (3-4)*8=-8', function() {
-    assert.equal(-8, (3-4)*8);
-  });
+  describe('Page Rendering Tests', () => {
+    describe('Page Title', () => {
+      const id = 'new-job-page-title';
 
+      it('checks the title text', () => {
+        return driver.findElement(By.id(id)).getText()
+            .then((text) => {
+              assert.equal(STRINGS[id], text);
+            });
+      });
+    });
 
-  it('changes button opacity on user input', async () => {
-    await driver.findElement(By.css('input')).sendKeys('us@goog.com')
-        .catch((err) => {
-          console.log('error:', err);
-        });
+    describe('Cancel Button', () => {
+      const id = 'new-job-cancel';
+
+      it('checks the value attribute', () => {
+        return driver.findElement(By.id(id)).getAttribute('value')
+            .then((value) => {
+              assert.equal(STRINGS[id], value);
+            });
+      });
+
+      it('checks the type attribute', () => {
+        return driver.findElement(By.id(id)).getAttribute('type')
+            .then((value) => {
+              assert.equal('reset', value);
+            });
+      });
+    });
+
+    describe('Submit Button', () => {
+      const id = 'new-job-submit';
+
+      it('checks the value attribute', () => {
+        return driver.findElement(By.id(id)).getAttribute('value')
+            .then((value) => {
+              assert.equal(STRINGS[id], value);
+            });
+      });
+
+      it('checks the type attribute', () => {
+        return driver.findElement(By.id(id)).getAttribute('type')
+            .then((value) => {
+              assert.equal('submit', value);
+            });
+      });
+    });
   });
 });
