0cfc78718e9b3dc68ad60fd61a15f3878b6588dd
Wu Sibing <1915563868@qq.com>
2020-07-13 16:09:35 +0800

    Add postalCode field to Location

--- walk-in-interview/src/main/java/com/google/job/data/Location.java
+++ walk-in-interview/src/main/java/com/google/job/data/Location.java
@@ -4,6 +4,7 @@ package com.google.job.data;
 public final class Location {
     // TODO(issue/23): add "postalCode" and deal with optional address
     private final String address;
+    private final long postalCode;
     private final double latitude;
     private final double longitude;
 
@@ -11,11 +12,12 @@ public final class Location {
 
     // For serialization
     public Location() {
-        this(/* address= */"", /* latitude= */0, /* longitude= */0);
+        this(/* address= */"", /* postalCode= */0, /* latitude= */0, /* longitude= */0);
     }
 
-    public Location(String address, double latitude, double longitude) {
+    public Location(String address, long postalCode, double latitude, double longitude) {
         this.address = address;
+        this.postalCode = postalCode;
         this.latitude = latitude;
         this.longitude = longitude;
     }
@@ -54,6 +56,7 @@ public final class Location {
         Location that = (Location) o;
         return Double.compare(that.latitude, latitude) == 0 &&
                 Double.compare(that.longitude, longitude) == 0 &&
+                that.postalCode == postalCode &&
                 address.equals(that.address);
     }
 
@@ -74,6 +77,9 @@ public final class Location {
         c = address.hashCode();
         result = 31 * result + c;
 
+        c = ((Long) postalCode).hashCode();
+        result = 31 * result + c;
+
         this.hashCode = result;
 
         return hashCode;
@@ -81,7 +87,7 @@ public final class Location {
 
     @Override
     public String toString() {
-        return String.format("JobLocation{address=%s, latitude=%f, longitude=%f}",
-                address, latitude, longitude);
+        return String.format("JobLocation{address=%s, postalCode=%d, latitude=%f, longitude=%f}",
+                address, postalCode, latitude, longitude);
     }
 }
