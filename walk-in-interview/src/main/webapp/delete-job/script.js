5fa16f4d26b067ddef43a440091b64fa5c7caa92
Wu Sibing <1915563868@qq.com>
2020-07-17 21:28:17 +0800

    Render button text dynamically; fix listener

--- walk-in-interview/src/main/webapp/delete-job/script.js
+++ walk-in-interview/src/main/webapp/delete-job/script.js
@@ -10,20 +10,30 @@
  */
 import {AppStrings} from "../strings.en.js";
 
+import {setErrorMessage} from "../common-functions.js";
+
 const STRINGS = AppStrings["delete-job"];
+const HOMEPAGE_PATH = "../job-details/index.html";
+const RESPONSE_ERROR = "There was an error while deleting the job post. Please try again";
+
+window.onload = () => {
+    renderDeleteButton();
+};
 
 /**
  * Clicks the button to mark this job post as DELETED.
- * 
- * @param {String} jobId Job id for this job post.
  */
-function deleteJobPost(jobId) {
+function renderDeleteButton() {
     const deleteButtonElement = document.getElementById("delete");
-
     deleteButtonElement.innerText = STRINGS["delete"];
-    deleteButtonElement.addEventListener("click", () => {
-        sendRequest(jobId);
-    });
+}
+
+/**
+ * Gets the job id from the url.
+ */
+function getJobId() {
+    // TODO(issue/53): gets the job Id
+    return "";
 }
 
 /**
@@ -31,8 +41,31 @@ function deleteJobPost(jobId) {
  * 
  * @param {String} jobId Job id for this job post.
  */
-function sendRequest(jobId) {
+function deleteJobPost(jobId) {
     const params = new URLSearchParams();
     params.append("jobId", jobId);
-    fetch("/jobs/delete", {method: "PATCH", body: params});
+    fetch("/jobs/delete", {method: "PATCH", body: params})
+    .then((response) => response.text())
+    .then((data) => {
+      console.log('data', data);
+      /** reset the error (there might have been an error msg from earlier) */
+      setErrorMessage(/* errorMessageElementId= */'error-message', /* msg= */ '', /* includesDefault= */false);
+      window.location.href= HOMEPAGE_PATH;
+    })
+    .catch((error) => {
+      setErrorMessage(/* errorMessageElementId= */'error-message', /* msg= */ RESPONSE_ERROR, /* includesDefault= */false);
+      console.log('error', error);
+    });
 }
+
+const deleteButtonElement = document.getElementById("delete");
+deleteButtonElement.addEventListener("click", () => {
+    const jobId = getJobId();
+    if (jobId === "") {
+        setErrorMessage(/* errorMessageElementId= */"error-message", /* msg= */ "Empty Job Id found. " + RESPONSE_ERROR,
+            /* includesDefault= */false);
+        return false;
+    }
+
+    deleteJobPost(getJobId());
+});
