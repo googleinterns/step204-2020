f56b253d6c16c8b7dca9a9a4a253acbbeb186eee
Wu Sibing <1915563868@qq.com>
2020-07-21 16:21:04 +0800

    Modify typo

--- walk-in-interview/src/main/webapp/common-functions.js
+++ walk-in-interview/src/main/webapp/common-functions.js
@@ -68,7 +68,7 @@ function setCookie(cname, cvalue, exdays=0.5) {
   var d = new Date();
   d.setTime(d.getTime() + (exdays*24*60*60*1000));
   var expires = `expires=${d.toUTCString()}`;
-  document.cookie = `${cname}=${cvalue};${expires};${path}=/`;
+  document.cookie = `${cname}=${cvalue};${expires};path=/`;
 }
 
 /**
