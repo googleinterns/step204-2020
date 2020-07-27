deb1a14f741ac171cad6725455dda723fcbdc28f
Riya Narayan <riya.narayan@gmail.com>
2020-07-27 20:43:39 +0800

    add info window

--- walk-in-interview/src/main/webapp/maps.js
+++ walk-in-interview/src/main/webapp/maps.js
@@ -8,6 +8,8 @@ const SG_LATITUDE = 1.3521;
 const SG_LONGITUDE = 103.8198;
 const SG_MAP_ZOOM = 11;
 
+const JOB_DETAILS_PATH = '/job-details/index.html';
+
 /**
  * Creates a map in the provided html div.
  * @param {String} mapElementId The map element id.
@@ -48,6 +50,26 @@ function addMarker(map, job) {
   });
   newJobMarker.addListener('click', function() {
     // TODO(issue/xx): somehow link to the job post in the list
+    createInfoWindow(job).open(map, newJobMarker);
+  });
+}
+
+/**
+ * This creates an info window for the marker.
+ * @param {Object} job The job that corresponds to the marker.
+ * @return {google.maps.InfoWindow} The info window created.
+ */
+function createInfoWindow(job) {
+  const pay = job['jobPay'];
+  const payStr = `${pay['min']} - ${pay['max']} SGD ` +
+  `(${pay['paymentFrequency'].toLowerCase()})`;
+
+  return new google.maps.InfoWindow({
+    content: `<form method="GET" action=${JOB_DETAILS_PATH}>` +
+    `<input type="hidden" name="jobId" value=${job['jobId']}` +
+    '<div style="font-weight:bold;">' + job['title'] +
+    '</div><div style="font-style:italic;">' + payStr + '</div>' +
+    `<input type"submit">Show Details</button></form>`,
   });
 }
 
