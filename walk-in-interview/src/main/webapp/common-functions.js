faf9cb6277c25c9b2c1cd4205c019c9e7106d85a
Wu Sibing <1915563868@qq.com>
2020-08-03 16:43:22 +0800

    Change to single quote

--- walk-in-interview/src/main/webapp/common-functions.js
+++ walk-in-interview/src/main/webapp/common-functions.js
@@ -63,13 +63,13 @@ function renderSelectOptions(select, options) {
 /**
  * Gets the value of a specified cookie.
  * 
- * @param {String} cname 
+ * @param {String} cname Cookie name.
  */
 function getCookie(cname) {
   var name = `${cname}=`;
   var decodedCookie = decodeURIComponent(document.cookie);
   var ca = decodedCookie.split(';');
-  for(var i = 0; i <ca.length; i++) {
+  for(var i = 0; i < ca.length; i++) {
     var c = ca[i];
     while (c.charAt(0) == ' ') {
       c = c.substring(1);
@@ -78,7 +78,7 @@ function getCookie(cname) {
       return c.substring(name.length, c.length);
     }
   }
-  return "";
+  return '';
 }
 
 export {JOB_ID_PARAM, getRequirementsList, setErrorMessage,
