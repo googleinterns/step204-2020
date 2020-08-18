77bc112a837ce054d947ae64e4134e538c4cb036
Wu Sibing <1915563868@qq.com>
2020-08-18 11:21:08 +0800

    Add createEmptyAccount

--- walk-in-interview/src/main/webapp/account/create-account/applicant/script.js
+++ walk-in-interview/src/main/webapp/account/create-account/applicant/script.js
@@ -12,14 +12,16 @@ const CURRENT_LOCALE = 'en';
  */
 import {AppStrings} from '../../../strings.en.js';
 import {Auth} from '../../firebase-auth.js';
+import {API} from '../../../apis.js';
 import {USER_TYPE_COOKIE_PARAM, USER_TYPE_APPLICANT, 
   setCookie} from '../../../common-functions.js';
 
 const AUTH_STRINGS = AppStrings['auth'];
-const COMMONG_STRINGS = AppStrings['create-account'];
+const ACCOUNT_STRINGS = AppStrings['create-account'];
 const STRINGS = AppStrings['create-applicant-account'];
 const CREATE_ACCOUNT_INFO_PAGE_PATH = './account-info/index.html';
 const CREATE_ACCOUNT_HOMEPAGE_PATH = '../index.html';
+const BAD_REQUEST_STATUS_CODE = 400;
 
 window.onload = () => {
   Auth.subscribeToUserAuthenticationChanges(
@@ -30,11 +32,13 @@ window.onload = () => {
 /**
  * What to do after the user signed in and the session cookie is created.
  */
-function onLogIn() {
+async function onLogIn() {
   // TODO(issue/101): Display button according to log in status;
 
   // TODO(issue/100): set the cookie at the server side instead
   setCookie(USER_TYPE_COOKIE_PARAM, USER_TYPE_APPLICANT);
+
+  await createEmptyAccount();
 
   // TODO(issue/102): replace with proper notification
   alert(STRINGS['new-user-info']);
@@ -63,10 +67,64 @@ function renderPageElements() {
   Auth.addPhoneSignInAndSignUpUI('phone-auth', CREATE_ACCOUNT_INFO_PAGE_PATH, STRINGS['new-user-info']);
 
   const backButton = document.getElementById('back');
-  backButton.innerText = COMMONG_STRINGS['back'];
+  backButton.innerText = ACCOUNT_STRINGS['back'];
 }
 
 const backButton = document.getElementById('back');
 backButton.addEventListener('click', (_) => {
   window.location.href = CREATE_ACCOUNT_HOMEPAGE_PATH;
 });
+
+/**
+ * Creates an account with phoneNumber as name for the user
+ */
+async function createEmptyAccount() {
+  var user = firebase.auth().currentUser;
+  if (!user) {
+    return new Promise.reject("Not signed in");
+  }
+
+  // Creates a totally empty account
+  const accountDetails = {
+    userType: USER_TYPE_APPLICANT,
+    name: user.phoneNumber,
+    // empty interest list
+  };
+
+  return fetch(API['create-applicant-account'], {
+    method: 'POST',
+    headers: {'Content-Type': 'application/json'},
+    body: JSON.stringify(accountDetails),
+  })
+      .then((response) => {
+        if (response.status == BAD_REQUEST_STATUS_CODE) {
+          setErrorMessage(/* errorMessageElementId= */'error-message',
+              /* msg= */ ACCOUNT_STRINGS['create-account-error-message'],
+              /* includesDefault= */false);
+          throw new Error(ACCOUNT_STRINGS['create-account-error-message']);
+        }
+
+        /** reset the error (there might have been an error msg from earlier) */
+        setErrorMessage(/* errorMessageElementId= */'error-message',
+            /* msg= */ '', /* includesDefault= */false);
+
+        // Enables the button regardless of success or failure
+        document.getElementById('submit').disabled = false;
+
+        // Directs to the page to fill in business account info.
+        window.location.href = CREATE_ACCOUNT_INFO_PAGE_PATH;
+      })
+      .catch((error) => {
+        // Not the server response error already caught and thrown
+        if (error.message != ACCOUNT_STRINGS['create-account-error-message']) {
+          console.log('error', error);
+
+          setErrorMessage(/* errorMessageElementId= */'error-message',
+              /* msg= */ ACCOUNT_STRINGS['error-message'],
+              /* includesDefault= */false);
+        }
+
+        // Enables the button regardless of success or failure
+        document.getElementById('submit').disabled = false;
+      });
+}
